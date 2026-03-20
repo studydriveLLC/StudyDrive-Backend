b5b17210b3fd848b1ac0a59ee5d6178b04612537
baticlean <baticlean225@gmail.com>
2026-03-20 12:13:32 +0000

    correction ENV

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
